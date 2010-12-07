6a99ec991ede2fcadf72d0c833dddb4924c379af
lyahim <lyahim@e9d61142-0182-3550-4773-6980dfa5f223>
2010-12-07 20:15:50 +0000

    Actionitem remove from hierarchy

--- GameServer/src/main/java/com/aionemu/gameserver/model/gameobjects/instance/ActionItem.java
+++ GameServer/src/main/java/com/aionemu/gameserver/model/gameobjects/instance/ActionItem.java
@@ -18,96 +18,99 @@
  */
 package com.aionemu.gameserver.model.gameobjects.instance;
 
-import com.aionemu.gameserver.model.EmotionType;
-import com.aionemu.gameserver.model.TaskId;
-import com.aionemu.gameserver.model.gameobjects.Creature;
 import com.aionemu.gameserver.model.gameobjects.interfaces.IDialogRequest;
 import com.aionemu.gameserver.model.gameobjects.interfaces.IReward;
 import com.aionemu.gameserver.model.gameobjects.player.Player;
-import com.aionemu.gameserver.model.templates.spawn.SpawnTemplate;
-import com.aionemu.gameserver.network.aion.serverpackets.SM_EMOTION;
-import com.aionemu.gameserver.network.aion.serverpackets.SM_LOOKATOBJECT;
-import com.aionemu.gameserver.network.aion.serverpackets.SM_USE_OBJECT;
-import com.aionemu.gameserver.questEngine.QuestEngine;
-import com.aionemu.gameserver.questEngine.model.QuestEnv;
-import com.aionemu.gameserver.services.DropService;
-import com.aionemu.gameserver.services.RespawnService;
-import com.aionemu.gameserver.utils.PacketSendUtility;
-import com.aionemu.gameserver.utils.ThreadPoolManager;
 
 /**
  * @author Mr. Poke, Lyahim
  *
  */
-public class ActionItem extends StaticNpc implements IReward, IDialogRequest
+public class ActionItem /*extends StaticNpc*/implements IReward, IDialogRequest
 {
 
-	/**
-	 * @param objId
-	 * @param spawnTemplate
-	 */
-	public ActionItem(int objId, SpawnTemplate spawnTemplate)
-	{
-		super(objId, spawnTemplate);
-	}
-
-	private Player	lastActor	= null;
-
-	/**
-	 * 0 - clear object
-	 * 1 - use object
-	 * 3 - convert object
+	/* (non-Javadoc)
+	 * @see com.aionemu.gameserver.model.gameobjects.interfaces.IDialogRequest#onDialogRequest(com.aionemu.gameserver.model.gameobjects.player.Player)
 	 */
 	@Override
-	public void onDialogRequest(final Player player)
+	public void onDialogRequest(Player player)
 	{
-		if (!QuestEngine.getInstance().onDialog(new QuestEnv(this, player, 0, -1)))
-			return;
-		final int defaultUseTime = 3000;
-		PacketSendUtility.sendPacket(player, new SM_USE_OBJECT(player.getObjectId(), getObjectId(), defaultUseTime, 1));
-		PacketSendUtility.broadcastPacket(player, new SM_EMOTION(player, EmotionType.START_QUESTLOOT, 0, getObjectId()), true);
-		ThreadPoolManager.getInstance().schedule(new Runnable()
-		{
-			@Override
-			public void run()
-			{
-				PacketSendUtility.sendPacket(player, new SM_USE_OBJECT(player.getObjectId(), getObjectId(), defaultUseTime, 0));
-				setTarget(player);
-				lastActor = player;
-				onDie(player);
-			}
-		}, defaultUseTime);
 	}
 
+	/* (non-Javadoc)
+	 * @see com.aionemu.gameserver.model.gameobjects.interfaces.IReward#doReward()
+	 */
 	@Override
 	public void doReward()
 	{
-		if (lastActor == null)
-			return;
-
-		DropService.getInstance().registerDrop(this, lastActor, lastActor.getLevel());
-		DropService.getInstance().requestDropList(lastActor, this.getObjectId());
-
-		lastActor = null;
 	}
-
-	@Override
-	public void onRespawn()
-	{
-		super.onRespawn();
-		DropService.getInstance().unregisterDrop(this);
-	}
-
-	@Override
-	public void onDie(Creature lastAttacker)
-	{
-		super.onDie(lastAttacker);
-
-		addTask(TaskId.DECAY, RespawnService.scheduleDecayTask(this));
-		scheduleRespawn();
-
-		// deselect target at the end
-		setTarget(null);
-		PacketSendUtility.broadcastPacket(this, new SM_LOOKATOBJECT(this));
-	}
+	//
+	//	/**
+	//	 * @param objId
+	//	 * @param spawnTemplate
+	//	 */
+	//	public ActionItem(int objId, SpawnTemplate spawnTemplate)
+	//	{
+	//		super(objId, spawnTemplate);
+	//	}
+	//
+	//	private Player	lastActor	= null;
+	//
+	//	/**
+	//	 * 0 - clear object
+	//	 * 1 - use object
+	//	 * 3 - convert object
+	//	 */
+	//	@Override
+	//	public void onDialogRequest(final Player player)
+	//	{
+	//		if (!QuestEngine.getInstance().onDialog(new QuestEnv(this, player, 0, -1)))
+	//			return;
+	//		final int defaultUseTime = 3000;
+	//		PacketSendUtility.sendPacket(player, new SM_USE_OBJECT(player.getObjectId(), getObjectId(), defaultUseTime, 1));
+	//		PacketSendUtility.broadcastPacket(player, new SM_EMOTION(player, EmotionType.START_QUESTLOOT, 0, getObjectId()), true);
+	//		ThreadPoolManager.getInstance().schedule(new Runnable()
+	//		{
+	//			@Override
+	//			public void run()
+	//			{
+	//				PacketSendUtility.sendPacket(player, new SM_USE_OBJECT(player.getObjectId(), getObjectId(), defaultUseTime, 0));
+	//				setTarget(player);
+	//				lastActor = player;
+	//				onDie(player);
+	//			}
+	//		}, defaultUseTime);
+	//	}
+	//
+	//	@Override
+	//	public void doReward()
+	//	{
+	//		if (lastActor == null)
+	//			return;
+	//
+	//		DropService.getInstance().registerDrop(this, lastActor, lastActor.getLevel());
+	//		DropService.getInstance().requestDropList(lastActor, this.getObjectId());
+	//
+	//		lastActor = null;
+	//	}
+	//
+	//	@Override
+	//	public void onRespawn()
+	//	{
+	//		super.onRespawn();
+	//		DropService.getInstance().unregisterDrop(this);
+	//	}
+	//
+	//	@Override
+	//	public void onDie(Creature lastAttacker)
+	//	{
+	//		super.onDie(lastAttacker);
+	//
+	//		addTask(TaskId.DECAY, RespawnService.scheduleDecayTask(this));
+	//		scheduleRespawn();
+	//
+	//		// deselect target at the end
+	//		setTarget(null);
+	//		PacketSendUtility.broadcastPacket(this, new SM_LOOKATOBJECT(this));
+	//	}
 }
